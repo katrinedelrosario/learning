4262869639c87566ca94d84b60a1cc054a53facf
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-01-08 18:10:52 +0100

    -

--- react/create-react-app/my-new-app-name/src/components/partials/footer/footer.jsx
+++ react/create-react-app/my-new-app-name/src/components/partials/footer/footer.jsx
@@ -3,7 +3,9 @@ import style from './footer.module.scss'
 const Footer = () => {
     return (
         <footer className={style.container}>
-            
+            <li>info</li>
+            <li>info</li>
+            <li>info</li>
         </footer>
     )
 }
