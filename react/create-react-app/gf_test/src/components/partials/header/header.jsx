a62d2b37cdc789f87b9ac0e7d442141595493337
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-01-09 13:05:26 +0100

    -

--- react/create-react-app/gf_test/src/components/partials/header/header.jsx
+++ react/create-react-app/gf_test/src/components/partials/header/header.jsx
@@ -3,8 +3,12 @@ import style from './header.module.scss'
 const Header = () => {
     return (
         <header className={style.container}>
-            <h1>grundforløbsprøve</h1>
-            <h2>webudvikleruddannelsen techcollege aalborg 2020</h2>
+          <div className='header-bg'>
+            <h1>klima <span>tossen</span></h1>
+            <h2>klima-tossen.dk</h2>
+            <p>"det er rigtigt, at vi hver især kun kan bidrage lidt til den samlede løsning."</p>
+            <p>"lige så rigtigt er det, at vi slet ikke kan nå målet uden at hver især bidrager en smule til det."</p>
+          </div>
         </header>
     )
 }
--- react/create-react-app/gf_test/src/components/partials/main/main.jsx
+++ react/create-react-app/gf_test/src/components/partials/main/main.jsx
@@ -3,9 +3,6 @@ import style from './main.module.scss'
 const Main = () => {
     return (
         <main className={style.container}>
-            <h2>"klimatossen" option 2</h2>
-            <h3>webudvikler uddannelsen grundforløbsprøve medio 2020.</h3>
-            <h2>cleanup</h2>
 
         </main>
     )
