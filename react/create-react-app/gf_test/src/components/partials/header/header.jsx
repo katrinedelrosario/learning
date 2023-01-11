89f82f8f17e5d22e061953dd9d085a0e38eb6d20
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-01-11 10:22:40 +0100

    -

--- react/create-react-app/gf_test/src/components/partials/header/header.jsx
+++ react/create-react-app/gf_test/src/components/partials/header/header.jsx
@@ -3,12 +3,13 @@ import style from './header.module.scss'
 const Header = () => {
     return (
         <header className={style.container}>
-          <div className='header-bg'>
+          <div>
+          <img src={require('../../../assets/images/Bg-Hero-Blue.png')} alt="header-bg" />
+          </div>
             <h1>klima <span>tossen</span></h1>
             <h2>klima-tossen.dk</h2>
             <p>"det er rigtigt, at vi hver især kun kan bidrage lidt til den samlede løsning."</p>
             <p>"lige så rigtigt er det, at vi slet ikke kan nå målet uden at hver især bidrager en smule til det."</p>
-          </div>
         </header>
     )
 }
