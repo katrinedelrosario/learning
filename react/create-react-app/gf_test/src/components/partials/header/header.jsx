ad13e3703f7a80c7211cd6ae33994dfcca3eb416
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-01-20 13:10:48 +0100

    -

--- react/create-react-app/gf_test/src/components/partials/header/header.jsx
+++ react/create-react-app/gf_test/src/components/partials/header/header.jsx
@@ -3,13 +3,13 @@ import style from './header.module.scss'
 const Header = () => {
     return (
         <header className={style.container}>
-          <div>
           <img src={require('../../../assets/images/Bg-Hero-Blue.png')} alt="header-bg" />
-          </div>
-            <h1>klima <span>tossen</span></h1>
+          <div className={style.text}>
+            <h1>klima<span>tossen</span></h1>
             <h2>klima-tossen.dk</h2>
-            <p>"det er rigtigt, at vi hver især kun kan bidrage lidt til den samlede løsning."</p>
-            <p>"lige så rigtigt er det, at vi slet ikke kan nå målet uden at hver især bidrager en smule til det."</p>
+            <p>"Det er rigtigt, at vi hver især kun kan bidrage lidt <br /> til den samlede løsning."</p>
+            <p>"Lige så rigtigt er det, at vi slet ikke kan nå målet <br /> uden at hver især bidrager en smule til det."</p>
+          </div>
         </header>
     )
 }
---
+++ react/create-react-app/gf_test/src/components/partials/nav/nav.jsx
@@ -0,0 +1,11 @@
+import style from './main.module.scss'
+
+const Main = () => {
+    return (
+        <main className={style.container}>
+
+        </main>
+    )
+}
+
+export {Main}
