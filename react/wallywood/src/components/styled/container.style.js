a387e1460f0b600bb162aedddfb988f2d27be97b
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-01-30 09:57:11 +0100

    sc

--- react/wallywood/src/components/styled/container.style.js
+++ react/wallywood/src/components/styled/container.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
 export const ContainerStyle = styled.section`
-	border: solid 1px #000;
-	background-color: ${props => props.bg};
+	border: solid 10px #000;
+	background-color: ${props => props.theme.colors.primary};
 `
---
+++ react/wallywood/src/components/styled/theme.js
@@ -0,0 +1,10 @@
+
+const theme = {
+    colors: {
+        primary: '#5C1F06',
+        secondary: '#D97852',
+    },
+    fonts: ['Titillium Web', 'Open Sans'],
+}
+
+export {theme}
