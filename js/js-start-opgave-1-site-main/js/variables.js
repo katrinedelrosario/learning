38c0136dd87f0d3a3d06bfd36ee5b869189898db
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-03-27 09:39:41 +0200

    -

---
+++ js/js-start-opgave-1-site-main/js/variables.js
@@ -0,0 +1,25 @@
+
+const a = 1234;
+const b = "1234";
+const c = 123.4;
+const d = [1, 2, 3, 4];
+const e = { type: "Fiat", model: "500", color: "white" };
+const f = true;
+const g = null;
+const h = undefined;
+const i = function () { };
+const j = new Date();
+
+console.log("variabel a er: " + typeof(a));
+console.log("variabel b er: " + typeof(b));
+console.log("variabel c er: " + typeof(c));
+console.log("variabel d er: " + typeof(d));
+console.log("variabel e er: " + typeof(e));
+console.log("variabel f er: " + typeof(f));
+console.log("variabel g er: " + typeof(g));
+console.log("variabel h er: " + typeof(h));
+console.log("variabel i er: " + typeof(i));
+console.log("variabel j er: " + typeof(j));
+
+
+
