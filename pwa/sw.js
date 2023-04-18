1608efdc102d58aa00c3745971c7c0a3be271a30
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2023-04-18 12:58:29 +0200

    sw updates

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,15 +1,48 @@
+//
+const staticCacheName = 'site-static'
+
+const assets = [
+	'./',
+	'./index.html',
+	'./js/app.js',
+	'./js/ui.js',
+	'./js/materialize.min.js',
+	'./css/styles.css',
+	'./css/materialize.min.css',
+	'./img/dish.png'
+]
+
 // install service worker
 self.addEventListener('install', event => {
-	console.log('Service Worker has been installed');
+	//console.log('service worker has been installed');
+
+	event.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+			//console.log('cahing all assets');
+			cache.addAll(assets)
+		})
+	)
 })
 
 // activate service worker
 self.addEventListener('activate', event => {
-	console.log('Service Worker has been activated');
-})
+	//console.log('service worker has been activated');
+	event.waitUntil(
+		caches.keys().then(keys => {
+			return Promise.all(keys
+				.)
+		})
+		)
+	})
 
 // fetch
 self.addEventListener('fetch', event => {
-	console.log('Fetch event', event)
+	//console.log('Fetch event', event)
+	console.log(event.request);
+	event.respondWith(
+		caches.match(event.request).then(cacheRes => {
+			return cacheRes || fetch(event.request)
+		})
+	)
 })
 
